525d634267ced1a58094d78986493af3ef9e2786
sovanator <sunham.kc@gmail.com>
2021-11-25 12:14:09 -0800

    added all methods that I can think of

--- curcularLinkedList.py
+++ curcularLinkedList.py
@@ -10,7 +10,6 @@ class CircularLinkedList():
 
     def print(self):
         tempNode = self.head
-        print('here', tempNode.value)
         s=""
         if self.head == None:
             print('Empty list')
@@ -18,7 +17,6 @@ class CircularLinkedList():
             print("single node ",self.head.value)
         else:
             while tempNode.next !=self.head:
-                # print("tempNode.value ", tempNode.value)
                 s=s+ str(tempNode.value) + "-->"
                 tempNode=tempNode.next
             s=s+str(self.tail.value)
@@ -76,6 +74,44 @@ class CircularLinkedList():
                 tempNode=tempNode.next
             node.next = tempNode.next
             tempNode.next = node
+    def removeFirst(self):
+        if self.head == self.head.next:
+            self.head = None
+            self.tail = None
+            return
+        self.head = self.head.next
+        self.tail.next = self.head
+    def removeLast(self):
+        if self.head ==self.head.next:
+            self.head = None
+            self.tail = None
+            return
+            
+        tempNode = self.head
+        count = 0
+        while count+1 < self.length():
+            tempNode = tempNode.next
+            count+=1
+        self.tail = tempNode
+        tempNode.next = self.head
+    def removeFromLocation(self, location):
+        if self.length() < location:
+            print("Out of bound")
+        elif location==0:
+            self.removeFirst()
+        elif location ==-1:
+            self.removeLast()
+        else:
+            count = 0
+            tempNode = self.head
+            while count < location-1:
+                count +=1
+                tempNode = tempNode.next
+            tempNode.next = tempNode.next.next  
+    def deleteSLL(self):
+        self.head = None
+        self.tail = None
+
 ll1= CircularLinkedList()
 # ll1.createCSLL(2)
 ll1.createCSLL(1)
@@ -84,7 +120,7 @@ ll1.addtoBegining(2)
 ll1.addtoBegining(3)
 ll1.addtoBegining(4)
 ll1.addtoEnd(20)
-ll1.print()
-print(ll1.length())
 ll1.addLocation(30, -1)
+ll1.print()
+ll1.deleteSLL()
 ll1.print()
